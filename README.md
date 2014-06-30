4fbdab0e3e2fc2fe9f3298eec46bb40d46797ab8
naoa <visionary.incident@gmail.com>
2014-06-30 14:19:20 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# Groonga regex function plugin
+Groongaに出力結果を整形する正規表現の関数を追加します。
+
+## 関数
+### regex_extract
+
+指定したカラムの文字列から正規表現のパターンに一致する文字列を抽出します。
+
+* 実行例
+
+```
+>select Entries --output_columns 'regex_extract(title, " (.*) ", "<extract>\\\\1</extract>")' --command_version 2
+[[0,0.0,0.0],[[[1],[["regex_extract","Object"]],["<extract>and</extract>"]]]]
+```
+
+* 入力形式
+
+| arg        | description |
+|:-----------|:------------|
+| 1     | カラムを指定 |
+| 2     | 抽出する正規表現パターン | 
+| 3   | 出力パターン(省略時は1つ目にマッチした文字列) |
+
+* 出力形式
+パターンにマッチするものがあれば、出力パターンに沿った文字列が出力されます。  
+パターンにマッチするものがなければ、入力したカラムの文字列が出力されます。
+
+
+### regex_replace
+
+指定したカラムの文字列から正規表現のパターンに一致する文字列を全置換します。
+
+* 実行例
+
+```
+>select Entries --output_columns 'regex_replace(title, " (.*) ", " or ")' --command_version 2
+[[0,0.0,0.0],[[[1],[["regex_replace","Object"]],["groonga or MySQL"]]]]
+```
+
+* 入力形式
+
+| arg        | description |
+|:-----------|:------------|
+| 1     | カラムを指定 |
+| 2     | 抽出する正規表現パターン | 
+| 3   | 置換する文字列 |
+
+* 出力形式
+置換された文字列が出力されます。  
+
+
+## インストール
+本プラグインは、Groongaの4.0.3以降のバージョンが必要です。
+
+本プラグインは、RE2ライブラリを利用しています。あらかじめ、これらのライブラリをインストールしてください。CentOSであれば、yumのepelリポジトリでインストールすることができます。
+
+    % yum -y install re2 re2-devel
+
+プラグインをビルドしてインストールしてください。
+
+    % sh autogen.sh
+    % ./configure
+    % make
+    % sudo make install
+
+Groongaのプラグインは共有ライブラリのため、ライブラリパスに注意してください。
+
+## 使い方
+
+Groongaのデータベースに``functions/regex``プラグインを登録してください。データベースごとにこの作業が1回だけ必要です。
+
+    % groonga DB
+    > register functions/regex
+
+これにより、Groongaのデータベースに上記の関数が登録されて、上記のコマンド/関数が利用できるようになります。
+
+## Author
+
+Naoya Murakami naoya@createfield.com
+
+## License
+
+LGPL 2.1. See COPYING-LGPL-2.1 for details.
+
---
+++ functions/regex.cpp
@@ -0,0 +1,99 @@
+/* 
+  Copyright(C) 2014 Naoya Murakami <naoya@createfield.com>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; version 2
+  of the License.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public
+  License along with this library; if not, write to the Free
+  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+  MA 02110-1301, USA
+
+*/
+
+#include <groonga/plugin.h>
+
+#include <iostream>
+#include <string>
+#include <re2/re2.h>
+
+using namespace std;
+
+#ifdef __GNUC__
+# define GNUC_UNUSED __attribute__((__unused__))
+#else
+# define GNUC_UNUSED
+#endif
+
+static grn_obj *
+func_regex_extract(grn_ctx *ctx, GNUC_UNUSED int nargs,
+                   GNUC_UNUSED grn_obj **args,
+                   grn_user_data *user_data)
+{
+  grn_obj *result;
+  char *input = GRN_TEXT_VALUE(args[0]);
+  char *pattern = GRN_TEXT_VALUE(args[1]);
+
+  string s = input;
+  if (args[2] == NULL) {
+    RE2::Extract(input, pattern, "\\1", &s);
+  } else {
+    RE2::Extract(input, pattern, GRN_TEXT_VALUE(args[2]), &s);
+  }
+
+  if ((result = grn_plugin_proc_alloc(ctx, user_data, GRN_DB_LONG_TEXT, 0))) {
+    GRN_TEXT_SET(ctx, result, s.c_str(), strlen(s.c_str()));
+  }
+
+  return result;
+}
+
+static grn_obj *
+func_regex_replace(grn_ctx *ctx, GNUC_UNUSED int nargs,
+                   GNUC_UNUSED grn_obj **args,
+                   grn_user_data *user_data)
+{
+  grn_obj *result;
+  char *input = GRN_TEXT_VALUE(args[0]);
+  char *pattern = GRN_TEXT_VALUE(args[1]);
+  char *replace = GRN_TEXT_VALUE(args[2]);
+
+  string s = input;
+  RE2::GlobalReplace(&s, pattern, replace);
+
+  if ((result = grn_plugin_proc_alloc(ctx, user_data, GRN_DB_LONG_TEXT, 0))) {
+    GRN_TEXT_SET(ctx, result, s.c_str(), strlen(s.c_str()));
+  }
+
+  return result;
+}
+
+grn_rc
+GRN_PLUGIN_INIT(GNUC_UNUSED grn_ctx *ctx)
+{
+  return GRN_SUCCESS;
+}
+
+grn_rc
+GRN_PLUGIN_REGISTER(grn_ctx *ctx)
+{
+  grn_proc_create(ctx, "regex_extract", -1, GRN_PROC_FUNCTION,
+                  func_regex_extract, NULL, NULL, 0, NULL);
+  grn_proc_create(ctx, "regex_replace", -1, GRN_PROC_FUNCTION,
+                  func_regex_replace, NULL, NULL, 0, NULL);
+
+  return ctx->rc;
+}
+
+grn_rc
+GRN_PLUGIN_FIN(GNUC_UNUSED grn_ctx *ctx)
+{
+  return GRN_SUCCESS;
+}
